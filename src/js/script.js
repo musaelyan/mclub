e080811977215bff0ab14fc3769b4f4f87141718
Sergey Popov <tovarov.piter@gmail.com>
2016-06-16 21:35:43 +0300

    Проверка работоспособности плагина

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
 (function() {
-  
+  fullpage.initialize('#fullpage', {
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    menu: '#menu',
+    css3: true
+  });
 })();
