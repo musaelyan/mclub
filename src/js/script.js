5c7989d31f68b75f80483cb2467260b8c72dd750
Sergey Popov <tovarov.piter@gmail.com>
2016-06-18 22:48:11 +0300

    JS для прокрутки экранов

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,29 @@
     'navigationPosition': 'right',
     'navigationColor': '#000000',
   });
+
+  // прокрутка к последнему экрану
+  var toLast = document.querySelectorAll('.to-last');
+
+  if (toLast) {
+    toLast = Array.prototype.slice.call(toLast, 0);
+
+    toLast.forEach(function(item) {
+      item.addEventListener('click', function(event) {
+        event.preventDefault();
+        fullpage.moveTo(5);
+      });
+    });
+  }
+
+  // прокрутка на 2 экран
+  var toNext = document.querySelector('.to-next');
+
+  if (toNext) {
+    toNext.addEventListener('click', function(event) {
+      event.preventDefault();
+      fullpage.moveTo(2);
+    });
+  }
+
 })();
