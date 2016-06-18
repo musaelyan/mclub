62bb6090d87d09f80bcd19ced068aeedc84bdada
Sergey Popov <tovarov.piter@gmail.com>
2016-06-18 23:13:48 +0300

    Исправил счётчик

--- src/js/script.js
+++ src/js/script.js
@@ -42,7 +42,6 @@
 
   function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
-    //var seconds = Math.floor((t / 1000) % 60);
     var minutes = Math.floor((t / 1000 / 60) % 60);
     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     var days = Math.floor(t / (1000 * 60 * 60 * 24));
@@ -51,7 +50,6 @@
       'days': days,
       'hours': hours,
       'minutes': minutes
-      //'seconds': seconds
     };
   }
 
@@ -61,7 +59,6 @@ function initializeClock(id, endtime) {
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
-  //var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
@@ -69,7 +66,6 @@ function initializeClock(id, endtime) {
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    //secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
@@ -81,8 +77,7 @@ function initializeClock(id, endtime) {
   }
 
   // Здесь задаётся дедлайн
-  // Первое число - дни
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  var deadline = new Date(2016, 5, 24);
 
   initializeClock('clockdiv', deadline);
 
